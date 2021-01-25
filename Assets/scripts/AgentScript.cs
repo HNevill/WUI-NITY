7c1cb7a7e05bf8125a2e93932e303e488366789d
Hannah Nevill <hfnevill71@gmail>
2021-01-25 15:22:18 +0000

    Added agents

---
+++ Assets/scripts/AgentScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentScript : MonoBehaviour
+{
+
+	public Transform goal;
+	public ParticleSystem explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      NavMeshAgent agent = GetComponent<NavMeshAgent>();
+      agent.destination = goal.position;
+    }
+
+
+    public void Explode()
+    {
+    	explosion.gameObject.transform.SetParent(null);
+    	explosion.Play();
+
+    	Destroy(gameObject);
+    	Destroy(explosion.gameObject, explosion.main.duration);
+    }
+}
