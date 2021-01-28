9ec2f8321dd9bbd0f3b4df15dca8211a01c579ea
Hannah Nevill <hfnevill71@gmail>
2021-01-28 20:13:10 +0000

    Spawn on navmesh
    Adding script so the agents spawn on the navmesh

---
+++ Assets/scripts/InstantiationExample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InstantiationExample : MonoBehaviour 
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject myPrefab;
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(myPrefab, transform.position , Quaternion.identity);
+
+        }
+    }
+}
+        
