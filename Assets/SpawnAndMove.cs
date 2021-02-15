3325a902b3ea020be99ac1f27ca70a10f803a556
Hannah Nevill <hfnevill71@gmail>
2021-02-15 17:44:01 +0000

    Not working
    Moved around the spawn and move script

--- Assets/SpawnAndMove.cs
+++ Assets/SpawnAndMove.cs
@@ -5,51 +5,50 @@ using UnityEngine.AI;
 
 public class SpawnAndMove : MonoBehaviour
 {
-    public float range = 10.0f;
+    public float range = 100.0f;
     bool found_mesh = false;
     private GameObject Safehouse;
 	private Transform Safety;
+	private NavMeshAgent agent;
 
-    bool RandomPoint(Vector3 center, float range, out Vector3 result)
-    {
-        for (int i = 0; i < 30; i++)
-        {
-            Vector3 randomPoint = center + Random.insideUnitSphere * range;
-            NavMeshHit hit;
-            if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
-            {
-                result = hit.position;
-                found_mesh = true;
-                return true;
-            }
-        }
+	private void Start()
+	{
+
+		agent = GetComponent<NavMeshAgent>();
+		Safehouse = GameObject.FindWithTag("target");
+		Debug.Log("found");
+		TeleportToRoad();
+	}
+
+    void RandomPoint(Vector3 center, float range, out Vector3 result)
+   {
+        Vector3 randomPoint = center + Random.insideUnitSphere * range;
+        NavMeshHit hit;
+
         result = Vector3.zero;
-        return false;
-    }
-    // Start is called before the first frame update
-        
-    // Update is called once per frame
-    void Update()
-    {
-    	        if (found_mesh == false)
+
+        while(!found_mesh)
         {
-            Vector3 point;
-            if (RandomPoint(transform.position, range, out point))
-            {
-                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
-                Debug.Log("did a thing");
-                NavMeshAgent agent = GetComponent<NavMeshAgent>();
-
-                agent.Warp(point);
-
-                Safehouse = GameObject.FindWithTag("target");
-     			Safety = Safehouse.transform;
-
-   				agent.destination = Safety.position;
-    
-
-            }
+        	if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+        	{
+            	result = hit.position;
+            	found_mesh = true;
+        	}
         }
+    }
         
+    void TeleportToRoad()
+    {
+        Vector3 point;
+        RandomPoint(transform.position, range, out point);
+        Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+        Debug.Log(point);
+
+        agent.Warp(point);
+			Safety = Safehouse.transform;
+
+		agent.destination = Safety.position;
+		//found_mesh = true;
+
     }
 }
--- Assets/scripts/InstantiationExample.cs
+++ Assets/scripts/InstantiationExample.cs
@@ -7,12 +7,13 @@ public class InstantiationExample : MonoBehaviour
 {
     // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
     public GameObject myPrefab;
+    public int noOfAgents = 40;
 
     // This script will simply instantiate the Prefab when the game starts.
     void Start()
     {
 
-        for (int i = 0; i < 40; i++)
+        for (int i = 0; i < noOfAgents; i++)
         {
             Instantiate(myPrefab, transform.position , Quaternion.identity);
 
